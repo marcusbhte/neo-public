c8a9ce03dbe258eb88c49a92c11db84e0716ff62
Marcus Almeida <marcus.bhte@gmail.com>
2024-04-24 12:17:37 -0300

    Update 01_prepara_desktop.sh

--- desk001/scripts/01_prepara_desktop.sh
+++ desk001/scripts/01_prepara_desktop.sh
@@ -1,39 +1,79 @@
 #!/usr/bin/env bash
+##                          _____                     _             _        
+##                         |_   _|                   | |           (_)      
+##       _ __   ___  ___     | | ___  ___ _ __   ___ | | ___   __ _ _  __ _ 
+##      | '_ \ / _ \/ _ \    | |/ _ \/ __| '_ \ / _ \| |/ _ \ / _` | |/ _` |
+##      | | | |  __/ (_) |   | |  __/ (__| | | | (_) | | (_) | (_| | | (_| |
+##      |_| |_|\___|\___/    \_/\___|\___|_| |_|\___/|_|\___/ \__, |_|\__,_|
+##                                                             __/ |        
+##                                                            |___/         
+##                                                                          
+## ---------------------------------------------
+## Autor:      Marcus Almeida
+## Data:       24/04/2024 
+## Finalidade: Laboratório Metabase sobre Docker
+## ---------------------------------------------
+## SO:         Linux Ubuntu 22.04 LTS
+## Machine:    neoserver
+## Usuário:    
+## Senha:      
+## UserMetabs: atendimento@neo.tec.br
+## ---------------------------------------------
+## Aplicações:
+## 
+# +-----------------------------------------------------------------------+
 var_time=1
 clear
 while true;do
 echo " "
+echo "                          _____                     _             _       "
+echo "                         |_   _|                   | |           (_)      "
+echo "       _ __   ___  ___     | | ___  ___ _ __   ___ | | ___   __ _ _  __ _ "
+echo "      | '_ \ / _ \/ _ \    | |/ _ \/ __| '_ \ / _ \| |/ _ \ / _` | |/ _` |"
+echo "      | | | |  __/ (_) |   | |  __/ (__| | | | (_) | | (_) | (_| | | (_| |"
+echo "      |_| |_|\___|\___/    \_/\___|\___|_| |_|\___/|_|\___/ \__, |_|\__,_|"
+echo "                                                             __/ |        "
+echo "                                                            |___/         "
+echo " "
 echo "SEJA BEM VINDO AO $0 DO UBUNTU!"
 echo " "
 echo "Escolha uma opção:
-      0 - Sair do sistema
-      1 - Instalar atualizações do sistema [ root ]
-	  2 - Limpa e remove pacotes desnecessários [ root ]
-	  3 - Remove os apps desnecessários [ root ]
-	  4 - Instala o antivirus Clam [ root ]
-	  5 - Instala pacotes úteis [ root ]
-	  6 - Instala/Atualiza o Navegador Sankhya [ user ]
-	  7 - Definir o papel de parede institucional [ user ]
-	  8 - Para a descoberta de impressoras [ root ]
-	  "
+	0 - Sair do sistema
+	1 - Instalar atualizações do sistema [ root ]
+	2 - Limpa e remove pacotes desnecessários [ root ]
+	3 - Remove os apps desnecessários [ root ]
+	4 - Instala o antivirus Clam [ root ]
+	5 - Instala pacotes úteis [ root ]
+	6 - Instala/Atualiza o Navegador Sankhya [ user ]
+	7 - Definir o papel de parede institucional [ user ]
+	8 - Para a descoberta de impressoras [ root ]"
 echo " "
 echo -n "Opção escolhida: "
 read opcao
 case $opcao in
         1)
+				clear
                 echo "Verificando por novas atualizações..."
+				echo ""
+				echo "------------------------------------------------------"
                 sleep $var_time
                 apt update && apt upgrade -y
 				freshclam
                 ;;
         2)
+				clear
                 echo "Limpando e removendo pacotes desnecessários"
+				echo ""
+				echo "------------------------------------------------------"
                 sleep $var_time
                 apt autoclean
 				apt autoremove
                 ;;
         3)
+				clear
                 echo "Desinstalando pacotes que não serão utilizados"
+				echo ""
+				echo "------------------------------------------------------"
                 sleep $var_time
                 apt purge -y libreoffice*
 				apt purge -y pix 
@@ -52,17 +92,24 @@ case $opcao in
 				apt autoremove
                 ;;
         4)
+				clear
                 echo "Instalando o antivirus Clam..."
+				echo ""
+				echo "------------------------------------------------------"
 				apt -y install clamav 
 				apt -y install clamav-daemon
 				apt -y install clamtk
 				freshclam
                 ;;
         5)
+				clear
                 echo "Instalando pacotes úteis"
+				echo ""
+				echo "------------------------------------------------------"
                 sleep $var_time
 				apt install -y cups-pdf
 				apt install -y wget 
+				apt install -y tasksel 
 				apt install -y gcc-9 
 				apt install -y unzip 
 				apt install -y libssl-dev 
@@ -71,9 +118,13 @@ case $opcao in
 				apt update && apt upgrade -y
 				apt autoremove -y
 				apt autoclean -y
+				tasksel install ssh-server
                 ;;
         6)
+				clear
                 echo "Instalando o Navegador Sankhya"
+				echo ""
+				echo "------------------------------------------------------"
                 sleep $var_time
 				cd ~/
 				mkdir Sankhya
@@ -92,7 +143,10 @@ case $opcao in
 				chmod +x Sankhya
                 ;;
         7)
+				clear
                 echo "Definindo o papel de parede"
+				echo ""
+				echo "------------------------------------------------------"
                 sleep $var_time
 				cd ~/
 				cd Sankhya
@@ -115,7 +169,10 @@ case $opcao in
 				gsettings set org.gnome.desktop.background picture-uri "file://$WALLPAPERS/${ALIST[$number]}"
                 ;;
         8)
+				clear
                 echo "Parando a descoberta de impressoras"
+				echo ""
+				echo "------------------------------------------------------"
                 sleep $var_time
 				chmod a-x /etc/init.d/avahi-daemon
 				chmod a-x /etc/init.d/cups-browsed
@@ -124,7 +181,8 @@ case $opcao in
 				service cups-browsed stop
                 ;;
         0)
-                echo Saindo do sistema...
+				clear
+                echo "Saindo do sistema..."
                 sleep $var_time
                 exit 0
                 ;;
